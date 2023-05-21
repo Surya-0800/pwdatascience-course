3a60f40de67d759cf8b4e88f401fdf2193311b07
Surya-0800 <120456481+Surya-0800@users.noreply.github.com>
2023-05-21 13:33:17 +0530

    Assignment 4

---
+++ Untitled22.py
@@ -0,0 +1,116 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# How do you comment code in Python? What are the different types of comments?
+
+
+# We can comment the string using # symbol,we can comment a docstring also using ''' This is a doc string'''
+
+
+# In[2]:
+
+
+# What are variables in Python? How do you declare and assign values to variables?
+
+# variables are used to store data values.we declare variable using =
+
+a = "efesf"
+
+
+# In[ ]:
+
+
+# # Q3. How do you convert one data type to another in Python?
+
+# we can convert using typecasting like definig the type of data type we want to convert like str,list,int etc
+
+# a = 234
+
+# to convert into string using str(a)
+
+
+# In[ ]:
+
+
+# Q4. How do you write and execute a Python script from the command line?
+
+# In terminal we go to the file that is saved and enter python filename.py it will execute the code 
+
+
+# In[3]:
+
+
+# Q5. Given a list my_list = [1, 2, 3, 4, 5], write the code to slice the list and obtain the sub-list [2, 3].
+
+
+my_list = [1, 2, 3, 4, 5]
+my_list = my_list[1:3]
+print(my_list)
+
+
+# In[2]:
+
+
+#  complex number is a number that comprises both a real part and an imaginary part
+# we represent it as 
+
+com = complex(3,4)
+print(com)
+
+
+# In[3]:
+
+
+# Q7. What is the correct way to declare a variable named age and assign the value 25 to it?
+
+age = 25
+
+
+# In[5]:
+
+
+# Declare a variable named price and assign the value 9.99 to it. What data type does this variable
+# belong to?
+
+price = 9.99
+
+# this belongs to float data type
+
+
+# In[8]:
+
+
+# Q9. Create a variable named name and assign your full name to it as a string. How would you print the
+# value of this variable?
+
+name = "Surya"
+print(name)
+
+
+# In[9]:
+
+
+# Q10. Given the string "Hello, World!", extract the substring "World".
+
+string = "Hello, World!"
+substring = string[7:12]
+print(substring)
+
+
+# In[10]:
+
+
+# Create a variable named "is_student" and assign it a boolean value indicating whether you are
+# currently a student or not.
+
+is_student = False
+
+
+# In[ ]:
+
+
+
+
